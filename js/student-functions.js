39f3e071bdd0a7cca6daaad59480bed3e49ef7bf
Hadi Firdaus <hadi.firdaus@graduate.utm.my>
2026-06-27 11:08:51 +0800

    added comments to explain things

--- js/student-functions.js
+++ js/student-functions.js
@@ -1,7 +1,4 @@
 
-// S001 - Aina Rahman (Active)
-// aina@example.com
-// Active
 
 const student ={
     studentId: "S001",
@@ -10,16 +7,19 @@ const student ={
     status: "Active"
 };
 
+//normal function
 function formatStudent(student){
     // return formatted string
     return `${student.studentId} - ${student.studentName} (${student.status})`;
 }
 
+//arrow function
 const getStudentEmail = (student) => {
   // return student email
   return student.email;
 };
 
+//short arrow function
 const getStudentStatus = (student) => student.status;
 
 console.log(formatStudent(student));

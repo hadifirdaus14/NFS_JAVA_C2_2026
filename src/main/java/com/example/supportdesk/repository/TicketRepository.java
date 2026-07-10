81d9f655ebc168dc345988d4e7078887cd955089
Hadi Firdaus <hadi.firdaus@graduate.utm.my>
2026-07-10 13:01:56 +0800

    D7Ex2

---
+++ src/main/java/com/example/supportdesk/repository/TicketRepository.java
@@ -0,0 +1,10 @@
+package com.example.supportdesk.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.supportdesk.model.Ticket;
+
+public interface TicketRepository extends MongoRepository<Ticket, String> {
+    // Custom query methods can be defined here if needed
+    
+}

07f0e17924e1a346f1ee5a732e9152e1d42e84d5
Hadi Firdaus <hadi.firdaus@graduate.utm.my>
2026-07-24 09:56:46 +0800

    added frontend

---
+++ frontend/asset-tracker-ui/src/components/AppHeader.jsx
@@ -0,0 +1,10 @@
+export default function AppHeader() {
+    return (
+        <header className="app-header">
+            <div>
+                <p classname="eyebrow">Day 11 React Fundamentals</p>
+                <h1>Asset Tracker UI</h1>
+            </div>
+        </header>
+    );
+}
